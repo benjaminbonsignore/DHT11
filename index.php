68f33b80f538493da3503fe6d9dd7ea83ff92c4b
Benjamin Bonsignore <benjaminbonsignore@outlook.fr>
2018-11-30 14:42:18 +0100

    Envoi du dossier complet

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-<?php
-echo 'Hello world !';
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+	<h1>Température</h1>
+
+	<?php
+	echo 'Hello world !';
+	?>	
+</body>
+
+
