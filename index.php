9f5eda59e666a10a55ac7e1ef82a7febaec7e290
Benjamin Bonsignore <benjaminbonsignore@outlook.fr>
2018-11-30 15:49:02 +0100

    Ajout de tout (salut hamza)

--- index.php
+++ index.php
@@ -8,8 +8,25 @@
 	<h1>Température</h1>
 
 	<?php
-	echo 'Hello world !';
+	$json = file_get_contents('data.txt');
+	$json_data = json_decode($json,true);
+	
+	echo $json;
+	?>
+
+	<br><br>
+
+	<?php
+	echo "Il fait ".$json_data['temperature']."°C avec ".$json_data['humidite']."% d'humidité.";
 	?>	
+
+	<br><br>
+
+	<?php
+	echo "Le ".date("d/m/Y")." à ".date("H:i:s");
+	?>
+
+	<br><br>
 
 	<img src="img/thermometer.jpg">
 </body>
